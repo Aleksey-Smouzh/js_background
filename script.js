4186e8e975d9dcbd2039fe2c10a1668446ae3c36
Aleksey-Smouzh <xmz@tyt.by>
2023-08-18 10:58:41 +0200

    done

---
+++ script.js
@@ -0,0 +1,25 @@
+function lines() {
+
+
+  let sizeW = Math.random() * 12
+  let duration = Math.random() * 3
+  let e = document.createElement("div");
+  e.setAttribute("class", "line");
+  document.body.appendChild(e);
+e.style.width = 2 + sizeW + "px";
+e.style.left = Math.random() * + innerWidth + "px";
+e.style.animationDuration = 2 + duration + 's'
+
+setTimeout(function () {
+  document.body.removeChild(e);
+}, 5000);
+
+
+}
+// lines();
+
+
+
+setInterval(function () {
+  lines();
+}, 200);
